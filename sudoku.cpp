98176b200e1faab8604642e215ccc37a263042dc
dineshsrisai <dineshsrisai1@gmail.com>
2026-08-07 17:41:34 +0530

    Initialized the board

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,7 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	cout << "Sudoko";
+int main()
+{
+	int board[9][9] = {
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+	while (true)
+	{
+		for (int i = 0; i < 9; i++)
+		{
+			for (int j = 0; j < 9; j++)
+			{
+				cout << board[i][j] << " ";
+			}
+			cout << "\n";
+		}
+		cout << "\n"
+			 << "Enter a command : ";
+		string s;
+		getline(cin, s);
+
+		if (s.substr(0, 3) == "set")
+		{
+			board[s[5] - '1'][s[4] - 'a'] = s[7] - '0';
+		}
+
+		if (s == "exit")
+		{
+			break;
+		}
+		cout << s << "\n";
+	}
 	return 0;
 }
